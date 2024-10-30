fec2c7da48189556fd6fc6eaf89d6885ac61d668
German Sayago <german.sayago@gmail.com>
2024-10-30 12:13:57 -0300

    se agrego api y paginas para consultar

---
+++ app/pollsters/page.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Pollsters() {
+  return (
+    <div className="vh-100 d-flex align-items-center justify-content-center text-center">
+      <div>
+        <div className="card text-bg-success">
+          <div className="card-body p-4">
+            <h4 className="card-title text-uppercase">Persona Autorizada</h4>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
